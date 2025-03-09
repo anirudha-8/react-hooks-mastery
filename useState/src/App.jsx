726fab335738c2c633b502af6838b2a50dc3650a
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-09 23:54:09 +0530

    feat(useState): add useState Controlled Input example

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -2,13 +2,32 @@ import { useState } from "react";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const [userInput, setUserInput] = useState("");
+
 	return (
-		<div>
-			<h1>Click to Increment or Decrement the counter</h1>
-			<p>Counter: {count}</p>
-			<button onClick={() => setCount(count + 1)}>Increment</button>
-			<button onClick={() => setCount(count - 1)}>Decrement</button>
-		</div>
+		<>
+			<div>
+				<h1>Click to Increment or Decrement the counter</h1>
+				<p>Counter: {count}</p>
+				<button onClick={() => setCount(count + 1)}>Increment</button>
+				<button onClick={() => setCount(count - 1)}>Decrement</button>
+			</div>
+			<hr />
+			<div>
+				<h1>Controlled Input Example</h1>
+				<input
+					type="text"
+					placeholder="Enter something..."
+					value={userInput}
+					onChange={(e) => {
+						setUserInput(e.target.value);
+					}}
+				/>
+				<p>
+					Your text: <strong>{userInput}</strong>
+				</p>
+			</div>
+		</>
 	);
 }
 
