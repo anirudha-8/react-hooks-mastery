7a7304b2579b275c88072a562195f5d19ef71846
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-11 21:28:57 +0530

    feat(useEffect/Post.jsx): complete Post by fetching post title information.

--- 02-useEffect/src/components/Post.jsx
+++ 02-useEffect/src/components/Post.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 const Post = () => {
-	return <div>Post</div>;
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/posts")
+			.then((res) => res.json())
+			.then((data) => setData(data));
+	}, []);
+
+	return (
+		<div>
+			<h1>Post Title Information</h1>
+			<ul>
+				{data.map((post) => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 export default Post;
