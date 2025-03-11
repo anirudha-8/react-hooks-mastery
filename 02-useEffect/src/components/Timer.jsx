62851ce4a6aefafc8dc16e44adf56d7f0b5c0262
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-11 21:15:52 +0530

    feat(useEffect/Timer): complete Timer feature inside "Timer" component

--- 02-useEffect/src/components/Timer.jsx
+++ 02-useEffect/src/components/Timer.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+
 const Timer = () => {
-	return <div>Timer</div>;
+	const [timer, setTimer] = useState(0);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTimer((preTime) => preTime + 1);
+		}, 1000);
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+
+	return <div>Timer: {timer}</div>;
 };
 export default Timer;
