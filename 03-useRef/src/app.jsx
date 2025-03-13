c2646afa2e9be03d77d4fdd23a21f2baaf4d860c
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-13 10:16:47 +0530

    feat(03-useRef/app.jsx): add example for DOM manipulation and persisting values using "useRef"

--- 03-useRef/src/app.jsx
+++ 03-useRef/src/app.jsx
@@ -1,1 +1,24 @@
-export function App() {}
+import { useEffect, useRef, useState } from "react";
+
+export function App() {
+	const [userInput, setUserInput] = useState(null);
+	const userInputRef = useRef(null);
+	useEffect(() => {
+		userInputRef.current && userInputRef.current.focus();
+	}, []);
+	return (
+		<div>
+			<input
+				type="text"
+				name="text"
+				id="text"
+				value={userInput}
+				onChange={(e) => {
+					setUserInput(e.target.value);
+				}}
+				ref={userInputRef}
+			/>
+			<p>Typed text: {userInput}</p>
+		</div>
+	);
+}
