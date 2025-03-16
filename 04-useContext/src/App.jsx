ee9648d6d4d032e5bd7ce27caf53ea4588b35be0
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-16 11:51:12 +0530

    feat(04-useContext): add styles for "dark" and "light" theme modes

--- 04-useContext/src/App.jsx
+++ 04-useContext/src/App.jsx
@@ -1,2 +1,16 @@
-function App() {}
+import { useContext } from "react";
+import ThemeContext from "./context/ThemeContext";
+import App.css;
+
+function App() {
+	const { isDarkTheme, toggleTheme } = useContext(ThemeContext);
+	return (
+		<div className={`${isDarkTheme ? "dark-theme" : "light-thme"}`}>
+			<h1>{isDarkTheme ? "Dark" : "Light"} Theme.</h1>
+			<button onClick={toggleTheme}>
+				Change to {isDarkTheme ? "Dark" : "Light"} theme.
+			</button>
+		</div>
+	);
+}
 export default App;
