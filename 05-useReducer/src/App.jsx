0f20ef22f12b134ca810a0dee777110917bdde90
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-18 11:21:55 +0530

    feat(useReducer/App.jsx): add useReducer for "counter" functionality
    - counter functionality with "INCREMENT" feature
    - counter functionality with "DECREMENT" feature
    - counter functionality with "RESET" feature

--- 05-useReducer/src/App.jsx
+++ 05-useReducer/src/App.jsx
@@ -1,9 +1,31 @@
-import './App.css'
+import { useReducer } from "react";
+import "./App.css";
+
+const initialState = 0;
+
+const countReducer = (state, action) => {
+	switch (action.type) {
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;
+		case "RESET":
+			return 0;
+		default:
+			return state;
+	}
+};
 
 function App() {
-  return (
-    
-  )
+	const [count, dispatch] = useReducer(countReducer, initialState);
+	return (
+		<div>
+			<h1>Count: {count}</h1>
+			<button onClick={() => dispatch({ type: "INCREMENT" })}>INCREMENT</button>
+			<button onClick={() => dispatch({ type: "DECREMENT" })}>DECREMENT</button>
+			<button onClick={() => dispatch({ type: "RESET" })}>RESET</button>
+		</div>
+	);
 }
 
-export default App
+export default App;
