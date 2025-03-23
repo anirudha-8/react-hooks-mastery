4e3dad0a2fc453a8d192e978a0ae01cb9c0ad378
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-23 13:26:06 +0530

    feat(07-useId/components/Checkbox.jsx): add "Checkbox.jsx" component

---
+++ 07-useId/src/components/Checkbox.jsx
@@ -0,0 +1,9 @@
+const Checkbox = () => {
+	return (
+		<div>
+			<input type="checkbox" name="check" id="check" />
+			<label htmlFor="check">Terms & Conditions</label>
+		</div>
+	);
+};
+export default Checkbox;
