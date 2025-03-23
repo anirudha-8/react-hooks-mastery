cdef191c6256cf84d775c33317a574b29938b16f
Anirudha Bele <beleanirudha8@gmail.com>
2025-03-23 13:27:16 +0530

    feat(07-useId/App.jsx): add "Checkbox.jsx" component

--- 07-useId/src/App.jsx
+++ 07-useId/src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
+import "./App.css";
+import Checkbox from "./components/Checkbox";
 
 function App() {
-  return ()
+	return (
+		<div>
+			<Checkbox />
+		</div>
+	);
 }
 
-export default App
+export default App;
